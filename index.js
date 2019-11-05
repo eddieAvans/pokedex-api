9867f7e8087483a065089550f555913e518e3100
unknown <clwong@avans.nl>
2019-11-05 13:13:44 +0100

    cors

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ const routes = require('./routes/routes');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-})
 app.use('/api', routes);
 
 app.listen(process.env.PORT || 5000, () => console.log('Server started'));
--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,13 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 const handle = require('../helpers/handle');
 
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+});
+
 router.get('/', (req, res) => {
     res.json({
         message: 'Welcome to the pokedex API'
